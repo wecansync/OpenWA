c11a8857f1c91af383a49c6392fa9274f936148a
Fouad Salkini <fouad@wecansync.com>
2026-05-22 14:38:46 +0300

    Update message.module.ts

--- src/modules/message/message.module.ts
+++ src/modules/message/message.module.ts
@@ -1,23 +1,34 @@
-import { Module } from '@nestjs/common';
+import { Module, DynamicModule, Type } from '@nestjs/common';
 import { TypeOrmModule } from '@nestjs/typeorm';
-import { BullModule } from '@nestjs/bullmq';
 import { MessageService } from './message.service';
 import { BulkMessageService } from './bulk-message.service';
 import { MessageController } from './message.controller';
 import { SessionModule } from '../session/session.module';
 import { Message } from './entities/message.entity';
 import { MessageBatch } from './entities/message-batch.entity';
-import { ScheduledMessageProcessor } from '../queue/processors/scheduled-message.processor';
 import { QUEUE_NAMES } from '../queue/queue-names';
+
+// Only wire up BullMQ if queue support is enabled (mirrors the guard in app.module.ts).
+// This must be checked at module load time, so env-loader.ts must run first.
+const queueImports: Array<DynamicModule> = [];
+const queueProviders: Array<Type> = [];
+if (process.env.QUEUE_ENABLED === 'true') {
+  // eslint-disable-next-line @typescript-eslint/no-require-imports
+  const { BullModule } = require('@nestjs/bullmq');
+  // eslint-disable-next-line @typescript-eslint/no-require-imports
+  const { ScheduledMessageProcessor } = require('../queue/processors/scheduled-message.processor');
+  queueImports.push(BullModule.registerQueue({ name: QUEUE_NAMES.SCHEDULED_MESSAGE }));
+  queueProviders.push(ScheduledMessageProcessor);
+}
 
 @Module({
   imports: [
     TypeOrmModule.forFeature([Message, MessageBatch], 'data'),
     SessionModule,
-    BullModule.registerQueue({ name: QUEUE_NAMES.SCHEDULED_MESSAGE }),
+    ...queueImports,
   ],
   controllers: [MessageController],
-  providers: [MessageService, BulkMessageService, ScheduledMessageProcessor],
+  providers: [MessageService, BulkMessageService, ...queueProviders],
   exports: [MessageService, BulkMessageService],
 })
 export class MessageModule {}
